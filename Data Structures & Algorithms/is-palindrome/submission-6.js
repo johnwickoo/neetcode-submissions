cd71227cdac73347d44f913f487345ce1ecdbd13
Bukola <66728362+johnwickoo@users.noreply.github.com>
2026-04-16 17:03:06 +0100

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const cleanedCharacter = s
+        .replace(/[^a-z0-9]/gi, '')
+        .toLowerCase()
+        let left=0;
+        let right=cleanedCharacter.length - 1
+
+        while(left<right){
+            if(cleanedCharacter[left]!==cleanedCharacter[right]) return false;
+            left++;
+            right--;
+        }
+        return true
+    }
+}
